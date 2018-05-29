cb286dd7bf4ca8d0687106db26a77259de218f75
Wung <wungmathing@gmail.com>
2018-05-29 15:20:01 -0400

    :new: Add prod url

--- src/config/AppConst.js
+++ src/config/AppConst.js
@@ -1,3 +1,4 @@
+export const isProduction = process.env.NODE_ENV === "production"
 export default {
-  baseURL: "http://localhost:4000"
+  baseURL: isProduction ? process.env.REACT_APP_BASE_URL : "http://localhost:4000"
 }
